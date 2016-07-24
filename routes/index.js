f2b1781276e05337a6dbb86d54cb8c873b9a7a90
Alexander Murphy <alexandermurphy@Alexanders-MacBook-Pro.local>
2016-07-23 23:17:36 -0600

    db connected and json API serving property data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var queries = require('../lib/queries.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  queries.getSalesData().then((resp) => {
+    res.json(resp)
+  })
 });
 
 module.exports = router;
